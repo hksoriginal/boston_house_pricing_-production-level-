5965ba71aafe442de79dedaf506298dcc10d896a
hksoriginal <harshitkumar454@gmail.com>
2022-09-30 02:14:39 +0530

    created web app

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from cgitb import reset
 import pickle
 from flask import Flask,request,app,jsonify,render_template,url_for
 
@@ -25,6 +26,14 @@ def predict_api():
     output = reg_model.predict(new_data)
     print(output)
     return jsonify(output[0])
+
+@app.route("/predict",methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = reg_model.predict(final_input)[0]
+    return render_template('home.html',prediction_text= 'The Predicted price is {}'.format(output))
 
 if __name__ == '__main__':
     app.run(debug=True)
