065a888d98c01640b8f60f5076616af28f9070fc
hksoriginal <harshitkumar454@gmail.com>
2022-09-30 01:32:27 +0530

    created web app

---
+++ app.py
@@ -0,0 +1,30 @@
+import pickle
+from flask import Flask,request,app,jsonify,render_template,url_for
+
+import numpy as np
+import pandas as pd
+
+
+
+
+app = Flask(__name__)
+
+reg_model = pickle.load(open('regression.pkl','rb'))
+scaler = pickle.load(open('scaler.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route("/predict_api",methods=['POST'])
+def predict_api():
+    data = request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output = reg_model.predict(new_data)
+    print(output)
+    return jsonify(output[0])
+
+if __name__ == '__main__':
+    app.run(debug=True)
